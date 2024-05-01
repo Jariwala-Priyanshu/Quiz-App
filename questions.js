b836d8606a3baa0bfd906a0fd8baa1eb61264cea
Jariwala-Priyanshu <163970321+Jariwala-Priyanshu@users.noreply.github.com>
2024-05-01 17:53:10 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,41 @@
+const questions=[
+   {
+        "id":1,
+        "question":"Who is the Prime Minister of India ?",
+        "options":["Umar Musa Yar'adua", "Muhammed Buhari","Narendra Modi","No idea"],
+        "answer": 3
+    },
+    {
+        "id":2,
+        "question":"If today is Wednesday, After 4 day which day is there?",
+        "options":["Monday", "Saturday","Sunday", "Friday"],
+        "answer": 3
+    },
+    {
+        "id":3,
+        "question":"Who is the CEO  of Megaminds Technologies?",
+        "options":["Tanvi Patel", "Parth Surani","Nikunj Balar", "Akash Kathariya"],
+        "answer": 3
+    },
+    {
+        "id":4,
+        "question":"In Which Year Megaminds Company Found?",
+        "options":["2011", "2012","2013", "2014"],
+        "answer": 2
+    },
+    {
+        "id":3,
+        "question":"Evaluate 50 + 50 * 2 = ?",
+        "options":["200", "150", "50", "0"],
+        "answer": 2
+    },
+    {
+        "id":3,
+        "question":"Which month has 28 day ?",
+        "options":["Januaray", "March", "April", "February"],
+        "answer": 4
+    }
+]
+
+
+
